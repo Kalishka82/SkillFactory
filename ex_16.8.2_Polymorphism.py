427fc727148f49e404a51b2f4c246c9fa52f7d2c
Tatiana Kalinina <109298612+Kalishka82@users.noreply.github.com>
2022-09-14 14:18:59 +0300

    Imported classes from ex_16_8_2

---
+++ ex_16.8.2_Polymorphism.py
@@ -0,0 +1,23 @@
+from ex_16_8_2 import Rectangle, Square, Circle
+
+rect_1 = Rectangle(3, 4)
+rect_2 = Rectangle(10, 15)
+# print(rect_1.get_area(), rect_2.get_area()) # 12 150
+
+square_1 = Square(5)
+square_2 = Square(10)
+# print(square_1.get_area_square(), square_2.get_area_square())   # 25 100
+
+circle_1 = Circle(5)
+circle_2 = Circle(10)
+# print(circle_1.get_area_circle(), circle_2.get_area_circle())   # 78.5 314.0
+
+
+figures = [rect_1, rect_2, square_1, square_2, circle_1, circle_2]
+for figure in figures:
+    if isinstance(figure, Circle):
+        print(figure.get_area_circle())
+    elif isinstance(figure, Square):
+        print(figure.get_area_square())
+    else:
+        print(figure.get_area())
