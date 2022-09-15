763bc1b3541d44812a2c215f099e204ab4803584
Tatiana Kalinina <109298612+Kalishka82@users.noreply.github.com>
2022-09-15 11:57:14 +0300

    just task was added to the file

--- ex_16.9.2_TriangleArea.py
+++ ex_16.9.2_TriangleArea.py
@@ -1,3 +1,8 @@
+# Напишите код для описания геометрической фигуры.
+# Создайте класс «прямоугольник» с помощью метода init(). На выходе в консоли вам необходимо
+# получить площадь данной фигуры.
+# !!! выполнено на примере треугольника!!!
+
 class Triangle:
     def __init__(self, a, b, c):
         self.a = a
@@ -10,7 +15,7 @@ class Triangle:
     def get_perimeter(self):
         return self.a + self.b + self.c
 
-    def get_square_triangle(self):
+    def get_area_triangle(self):
         return ((self.get_perimeter() / 2 *
                  (self.get_perimeter() / 2 - self.a) *
                  (self.get_perimeter() / 2 - self.b) *
@@ -20,5 +25,4 @@ class Triangle:
 triangle_1 = Triangle(3, 4, 5)
 print(triangle_1)                           # Triangle: 3, 4, 5
 print(triangle_1.get_perimeter())           # 12
-print(triangle_1.get_square_triangle())     # 6.0
-
+print(triangle_1.get_area_triangle())       # 6.0
